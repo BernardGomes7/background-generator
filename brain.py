6117fb51599d8fa4f43e16ee02cb247736e776c4
Vallary <bernardgomes78@gmail.com>
2023-09-09 13:09:15 +0530

    added exclamation

--- brain.py
+++ brain.py
@@ -4,7 +4,7 @@ import os
 exec_path = os.getcwd()
 
 prediction = ImageClassification()
-#hehehehhehhe
+#hehehehhehhe!
 prediction.setModelTypeAsMobileNetV2()
 prediction.setModelPath(os.path.join(exec_path, 'mobilenet_v2-b0353104.pth'))
 prediction.loadModel()
