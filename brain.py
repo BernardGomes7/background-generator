17fcf57818e6ac65865fec515f8bd413338bbefe
Vallary <bernardgomes78@gmail.com>
2023-09-09 13:05:32 +0530

    reverting to heheh

--- brain.py
+++ brain.py
@@ -4,6 +4,7 @@ import os
 exec_path = os.getcwd()
 
 prediction = ImageClassification()
+#hehehehhehhe
 prediction.setModelTypeAsMobileNetV2()
 prediction.setModelPath(os.path.join(exec_path, 'mobilenet_v2-b0353104.pth'))
 prediction.loadModel()
@@ -16,3 +17,4 @@ for eachPred, eachProb in zip(predctions, probabilities):
 
 
 #jfcsjkd;lcvjkfhsnd;d;ljk
+
